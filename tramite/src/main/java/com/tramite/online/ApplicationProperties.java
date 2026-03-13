2974535cd7af9ee1f7e2db8e08d5c033c08e7c40
David Garcia <david.garcia@grupomost.com>
2026-03-12 21:02:44 -0300

    add application properties yaml others

---
+++ tramite/src/main/java/com/tramite/online/ApplicationProperties.java
@@ -0,0 +1,16 @@
+package com.tramite.online;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+
+@ConfigurationProperties(prefix = "app")
+public record ApplicationProperties(
+
+        @DefaultValue("dgarcia")   String issuer,
+        public record OpenApiProperties(
+                @DefaultValue("Tramite Online API") String title,
+                @DefaultValue("1.0.0") String version
+        )
+) {
+}

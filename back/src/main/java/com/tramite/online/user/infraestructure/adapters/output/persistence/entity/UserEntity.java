2e4c3795d6d05e86650ce801949c47dc43af1596
david garcia <manuonda@gmail.com>
2026-05-31 01:02:57 -0300

    Feat(persistence): Complete UserEntity definition for JPA

--- back/src/main/java/com/tramite/online/user/infraestructure/adapters/output/persistence/entity/UserEntity.java
+++ back/src/main/java/com/tramite/online/user/infraestructure/adapters/output/persistence/entity/UserEntity.java
@@ -1,6 +1,6 @@
 package com.tramite.online.user.infraestructure.adapters.output.persistence.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="users")
@@ -10,9 +10,65 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 50)
     private String username;
+
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
     
+
+    public UserEntity() {
+    }
+
+    public UserEntity(Long id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public UserEntity(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     
 
 }
